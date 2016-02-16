d847aae8e5abd30b0902e40d177bd7baa15b50f2
gdantas05 <contatogdantas@gmail.com>
2016-02-16 04:26:58 +0000

    Módulo 3 do projeto

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,13 +10,30 @@
 | and give it the controller to call when that URI is requested.
 |
 */
+Route::pattern('id', '[0-9]+'); //CRIA UMA EXPRESSÃO REGULAR, QUALQUER PARÂMETRO COM O NOME ID DEVE SER NUMÉRICO COM PELO MENOS UM DÍGITO
+
+Route::get('user/{id?}', function($id = 123){
+   if ($id)
+    return "Olá $id";
+    
+   return "Não possui id";        
+});
+
+Route::group(['prefix' => 'admin'], function() {
+   Route::get('products', ['as'=>'produtos', 'AdminProductsController@index']); 
+   Route::get('categories',['as'=>'categorias', 'AdminCategoriesController@index']);
+});
+
+Route::get('category/{category}', function (CodeCommerce\Category $category) {
+   return $category->name; 
+});
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('admin/categories', 'AdminCategoriesController@index');
-Route::get('admin/products', 'AdminProductsController@index');
+//Route::get('admin/categories', 'AdminCategoriesController@index');
+//Route::get('admin/products', 'AdminProductsController@index');
 
 /*
 |--------------------------------------------------------------------------
